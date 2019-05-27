e36285f4342141802607545fac73b2c007d5a815
wdxtub <wdxtub@qq.com>
2019-05-28 00:44:00 +0800

    add code

--- advanced-analytics-with-spark/spark-shell/StatsWithMissing.scala
+++ advanced-analytics-with-spark/spark-shell/StatsWithMissing.scala
@@ -1,5 +1,30 @@
 import org.apache.spark.util.StatCounter
 
 class NAStatCounter extends Serializable {
+    val stats: StatCounter = new StatCounter()
+    var missing: Long = 0
 
+    def add(x: Double): NAStatCounter = {
+        if (java.lang.Double.isNaN(x)) {
+            missing += 1
+        } else {
+            stats.merge(x)
+        }
+        this
+    }
+
+    def merge(other: NAStatCounter): NAStatCounter = {
+        stats.merge(other.stats)
+        missing += other.missing
+        this
+    }
+
+    override def toString = {
+        "stats: " + stats.toString + " NaN: " + missing
+    }
+
+    // 一个伴生对象
+    object NAStatCounter extends Serializable {
+        def apply(x: Double) = new NAStatCounter().add(x)
+    }
 }
